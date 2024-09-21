c31d4429f0b16f2c3a50e1e4b46e9f4a94fba71b
manhavn <manhavn@outlook.com>
2024-09-21 08:50:35 +0700

    add svg read qrcode

--- go.mod
+++ go.mod
@@ -7,12 +7,16 @@ require (
 	github.com/makiuchi-d/gozxing v0.1.1
 	github.com/pquerna/otp v1.4.0
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c
+	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef
 	github.com/variar/buckets v0.0.1
 )
 
 require (
 	github.com/boombuler/barcode v1.0.2 // indirect
 	go.etcd.io/bbolt v1.3.11 // indirect
+	golang.org/x/image v0.10.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
--- lib/otp2fa/readqrcodeimage.go
+++ lib/otp2fa/readqrcodeimage.go
@@ -6,6 +6,9 @@ import (
 	"image/jpeg"
 	"image/png"
 
+	"github.com/srwiley/oksvg"
+	"github.com/srwiley/rasterx"
+
 	"github.com/makiuchi-d/gozxing"
 	"github.com/makiuchi-d/gozxing/qrcode"
 )
@@ -15,7 +18,16 @@ func ReadQrCodeImage(data []byte) (string, error) {
 	var err error
 	if imgData, err = png.Decode(bytes.NewReader(data)); err != nil {
 		if imgData, err = jpeg.Decode(bytes.NewReader(data)); err != nil {
-			return "", err
+			w, h := 256, 256
+			var icon *oksvg.SvgIcon
+			icon, err = oksvg.ReadIconStream(bytes.NewReader(data))
+			if err != nil {
+				return "", err
+			}
+			icon.SetTarget(0, 0, float64(w), float64(h))
+			rgba := image.NewRGBA(image.Rect(0, 0, w, h))
+			icon.Draw(rasterx.NewDasher(w, h, rasterx.NewScannerGV(w, h, rgba, rgba.Bounds())), 1)
+			imgData = rgba
 		}
 	}
 	// Prepare BinaryBitmap
